7046797e8b05cf4714d7c486483d8c70567556a4
Korey Nichols <koreynichols1@gmail.com>
2022-07-09 00:15:41 -0700

    init setup

--- geekbooks/geekbooks/settings.py
+++ geekbooks/geekbooks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lyyfm)om=(5laclsovsex8k&8n9xx7f&9fe%84t3+gvtj688e%'
+SECRET_KEY = 'django-insecure-*58*p!=534$uzpzkmb91!_zk*n0z7%hinzw2-jz&=7jz&@-=_g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
