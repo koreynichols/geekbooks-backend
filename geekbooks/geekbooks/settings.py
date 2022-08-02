d6619adb45755a73d21d359619f8a5fc06d70c10
Korey Nichols <koreynichols1@gmail.com>
2022-08-01 17:21:19 -0700

    restarted build for backend

--- geekbooks/geekbooks/settings.py
+++ geekbooks/geekbooks/settings.py
@@ -1,3 +1,4 @@
+from .secret_settings import *
 """
 Django settings for geekbooks project.
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*58*p!=534$uzpzkmb91!_zk*n0z7%hinzw2-jz&=7jz&@-=_g'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
