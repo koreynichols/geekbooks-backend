c7e4d7f3ceb3c22717af9a906825452e04cfb360
Korey Nichols <koreynichols1@gmail.com>
2022-08-12 01:51:36 -0700

    working on getting favorites api to work. Looks to be working, but may need to fix later on

--- geekbooks/favorite/views.py
+++ geekbooks/favorite/views.py
@@ -1,13 +1,72 @@
-from rest_framework import viewsets
+# from rest_framework import viewsets, status
+# from .serializers import FavoriteSerializer
+# from .models import Favorite
+# from rest_framework import generics
+# from rest_framework.response import Response
+# from rest_framework.decorators import action
+
+
+
+# class FavoriteViewset(generics.ListCreateAPIView):
+#     model = Favorite
+#     serializer_class = FavoriteSerializer
+
+#     @action(detail=True, methods=['POST'])
+#     def set_favorite(self, request):
+#         # return Response("ok")
+#         user_id = self.request.user
+#         book_id = request.book
+
+#         try:
+#             favorite = Favorite.objects.get(user=user_id, book = book_id)
+#             favorite.delete();
+#             response = {'message' : 'Book Favorite deleted'}
+#         except:
+#             Favorite.objects.create(book = book_id, user = user_id, is_favorite = 'true')
+#             response = {'message': 'Book Favorite created'}
+#         return Response(response, status=status.HTTP_200_OK)
+
+
+#     # def get_queryset(self):
+#     #     user_id = self.request.user
+
+#     #     user_favorites = Favorite.objects.filter(user=user_id)
+#     #     return (user_favorites)
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions, status
 from .serializers import FavoriteSerializer
 from .models import Favorite
+import json
+from .models import Book
 
-class FavoriteViewset(viewsets.ModelViewSet):
+class FavoriteViewset(APIView):
+
     model = Favorite
     serializer_class = FavoriteSerializer
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAdminUser]
 
-    def get_queryset(self):
-        user_id = 1
+    def get(self, request, format=None):
+        user_id = self.request.user
 
         user_favorites = Favorite.objects.filter(user=user_id)
-        return (user_favorites)
+        data = list(FavoriteSerializer(user_favorites, many=True).data)
+
+        return Response(data)
+
+
+    def post(self, request, format=None):
+        user_id = self.request.user
+        book_id = Book.objects.get(id = request.POST.get('book'))
+
+        try:
+            favorite = Favorite.objects.get(user=user_id, book = book_id)
+            favorite.delete();
+            response = {'message' : 'Book Favorite deleted'}
+        except:
+            Favorite.objects.create(book = book_id, user = user_id, is_favorite = 'True')
+            response = {'message': 'Book Favorite created'}
+        return Response(response, status=status.HTTP_200_OK)
--- geekbooks/geekbooks/urls.py
+++ geekbooks/geekbooks/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from rest_framework_simplejwt import views as jwt_views
+
 from book import views as book_views
 from review import views as review_views
 from favorite import views as favorite_views
@@ -9,9 +11,12 @@ from favorite import views as favorite_views
 router = routers.DefaultRouter()
 router.register(r'book', book_views.BookViewset)
 router.register(r'review', review_views.ReviewViewset)
-router.register(r'favorite', favorite_views.FavoriteViewset, basename="favorites")
+# router.register(r'favorite', favorite_views.FavoriteViewset.as_view(), basename="favorites")
 
 urlpatterns = [
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/favorites/', favorite_views.FavoriteViewset.as_view(), name="favorite"),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
