0b60bf6ab52a90f7fe67695b4ed9597bc233f0b2
Korey Nichols <koreynichols1@gmail.com>
2022-08-15 16:24:23 -0700

    google books search is working

---
+++ geekbooks/api/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import requests
+import json
+
+@api_view(['POST'])
+def index(request):
+    url = 'https://www.googleapis.com/books/v1/volumes?'
+
+    req_data = request.data
+
+    for key in req_data:
+        url += key + '='
+        url += req_data[key] + '+'
+
+    url = url[:-1]
+    url = url.replace(' ', '')
+
+    api_request = requests.get(url)
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error, data not loading"
+    
+    return Response(api, content_type="application/json")
--- geekbooks/geekbooks/urls.py
+++ geekbooks/geekbooks/urls.py
@@ -7,17 +7,17 @@ from rest_framework_simplejwt import views as jwt_views
 from book import views as book_views
 from review import views as review_views
 from favorite import views as favorite_views
+from api import views as api_views
 
 router = routers.DefaultRouter()
 router.register(r'book', book_views.BookViewset)
-# router.register(r'review', review_views.ReviewViewset)
-# router.register(r'favorite', favorite_views.FavoriteViewset.as_view(), basename="favorites")
 
 urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/favorites/', favorite_views.FavoriteViewset.as_view(), name="favorite"),
     path('api/reviews/', review_views.ReviewViewset.as_view(), name="review"),
+    path('google-api/', api_views.index, name='index'),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
